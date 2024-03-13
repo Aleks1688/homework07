dabeba7a6698c45fa7a1298bf374f80653deb7b4
Sasha <sfh1688@qq.com>
2024-03-13 22:19:20 +0800

    honework07:task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -2,3 +2,32 @@
 //Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 //Использовать рекурсию, не использовать циклы.
 
+void PrintNumbersFromMtoN(int m, int n)
+{
+
+    if (m < n)
+    {
+        if (m > n) return;
+        Console.Write($"{m} ");
+        PrintNumbersFromMtoN(m + 1, n);
+    }
+    else if (m > n)
+    {
+        if (m < n) return;
+        Console.Write($"{m} ");
+        PrintNumbersFromMtoN(m - 1, n);
+    }
+    else
+    {
+        Console.WriteLine($"{n}");
+    }
+
+}
+
+Console.WriteLine("Enter first digit: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second digit: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersFromMtoN(firstDigit, secondDigit);
