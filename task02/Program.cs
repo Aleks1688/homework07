a04c781e0dc57aa43b6f6681f974b50a1269b504
Sasha <sfh1688@qq.com>
2024-03-16 14:03:31 +0800

    homework07:task02

--- task02/Program.cs
+++ task02/Program.cs
@@ -15,3 +15,31 @@
 //      вернуть ack(n - 1, ack (n, m - 1))
 
 /***********************************************************/
+
+int AkkermanFunc(int n, int m)
+{
+    int result;
+    if (n == 0)
+    {
+        result = m + 1;
+    }
+    else if (m == 0)
+    {
+       result=AkkermanFunc((n -1), 1);
+    }
+    else
+    {
+        result=AkkermanFunc((n - 1), (AkkermanFunc(n, (m - 1))));
+    }
+    return result;
+}
+
+Console.WriteLine("Enter n digit: ");
+int nDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter m digit: ");
+int mDigit = Convert.ToInt32(Console.ReadLine());
+
+int akkerman = AkkermanFunc(nDigit, mDigit);
+
+Console.WriteLine(akkerman);
